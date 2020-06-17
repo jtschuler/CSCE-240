52b1f5dcda0144a83241bdb3c87caca83c9b59ee
jtschuler <jtschuler007@yahoo.com>
2020-06-17 15:28:05 -0400

    edits

--- Homework/hw5/inc/gradebook.h
+++ Homework/hw5/inc/gradebook.h
@@ -1,5 +1,75 @@
+// Copyright 2020 Jadon T Schuler
+
+#ifndef INC_GRADEBOOK_H_
+#define INC_GRADEBOOK_H_
+
+
 class GradeBook {
  public:
+  /* May or may not be required depending on how you manage memory. You may
+   * delete if unused.
+   */
+  GradeBook();
+
+  /* Class destructor to free dynamically allocated memory
+   */
+  ~GradeBook();
+
+  /* Adds a new grade to the gradebook instance, increasing size by 1.
+   */
+  void Add(char grade);
+
+
+  /* Returns the grade at the supplied index. When that index is out of range, a
+   * value of \0 is returned.
+   */
+  char Get(size_t index) const;
+
+
+  /* Adds two GradeBook instances together and returns their join as a new
+   * object.
+   * Order is maintained by adding the calling instance's grades to the new
+   * Gradebook instance, followed by the parameter GradeBook's instances.
+   */
+  const GradeBook Add(const GradeBook& that) const;
+
+
+  /* Returns the average of all grades stored in GradeBook instance, or \0 if
+   * the the GradeBook instance is empty.
+   */
+  char GetGPA() const;
+
+
+  /* Returns the max grade in the GradeBook or \0 if the calling instance is
+   * empty.
+   */
+  char GetMax() const;
+
+
+  /* Returns the number of grades currently stored by the GradeBook.
+   */
+  const size_t GetSize() const;
+
+
+  /* Returns true when two GradeBook instances have the same average grade
+   * value; returns false otherwise. An empty instance is treated as having an
+   * average grade value of -1.0---that is, less than F.
+   */
+  bool Equals(const GradeBook& that) const;
+
+
+  /* Returns true when the calling instance has a lower grade average than the
+   * paramter instance's average. If they are even, it compares their max
+   * grades. An empty instance is treated as having an average grade value of
+   * -1.0--that is, less than F.
+   */
+  bool LessThan(const GradeBook& that) const;
+
+
+  /* Beginning of new material
+   */
+
+
   /* Returns the grade at the supplied index. When that index is out of range, a
    * value of \0 is returned.
    */
@@ -37,4 +107,14 @@ class GradeBook {
    */
   bool operator<(const GradeBook& that) const;
 
+ private:
+  size_t size_;
+  char* grades_;
+  size_t grades_length_;
+
+  void Grow();
+  char NumToCharGPA(const double gpa) const;
+  double GradesToDoubleGPA() const;
 };
+
+#endif  // INC_GRADEBOOK_H_
